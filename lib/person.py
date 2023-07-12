c93c2f2991fd8269d2513871d540f443ff19841b
Giselle <gisellec60@gmail.com>
2023-07-12 17:40:06 -0400

    passed

--- lib/person.py
+++ lib/person.py
@@ -24,7 +24,7 @@ class Person:
         return self._name
     
     def set_name(self, name):
-        if (type(name) == str) and ( 1 <= len(name) <= 25) :
+        if ((type(name) == str) and ( 1 <= len(name) <= 25)) :
             self._name = name
         else:
             print("Name must be string between 1 and 25 characters.")
@@ -34,22 +34,25 @@ class Person:
         return self._name
     
     def set_name(self, name):
-        if   (1 <= len(name) <= 25) :
+        if (type(name) == str) and (1 <= len(name) <= 25) :
             self._name = name.title()
         else:
             print("Name must be string between 1 and 25 characters.")
     name=property(get_name,set_name)         
 
     def get_job(self):
-        return self._job
+        return self._job 
     
     def set_job(self, job):
-        if job in APPROVED_JOBS:
-            self._job = job
-        else:
+        if job not in APPROVED_JOBS:
             print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
 
     job = property(get_job, set_job)   
+
+    def get_job(self):
+        return self._job
 
     def set_job(self, job):
         if job in APPROVED_JOBS:
